a0602fd1a0943965fe750987c6ca53fbf7971ff2
needl3 <anishchapagai0@gmail.com>
2021-11-18 17:17:39 +0545

    minor changes

--- script.sh
+++ script.sh
@@ -1,99 +1,117 @@
 #!/usr/bin/bash
 
-cp .xinitrc /home/$SUDO_USER
-cp .bashrc /home/$SUDO_USER
+# All pacman packages to install
+packages_pacman=(socat openvpn qterminal xorg-xsetroot, neofetch, htop)
 
-
-# Program killer using name-----------------------------------------------------------
-# Usage: killProgram <process_name>
-echo "ps -A | grep $1 | xargs kill 2> /dev/null" > /usr/local/bin/killProgram
-chmod +x /usr/local/bin/killProgram
+# All yay packages to install
+packages_yay = ()
 
 # Directory containing all source files
 src_dir="src"
 
-# Conservation mode toggler------------------------------------------------------------
-g++ $(src_dir)/toggleConservation.cpp -o /usr/local/bin
-sudo chown root toggleConservationMode
-sudo chmod u+s toggleConservationMode
+placeFiles(){
+	cp .xinitrc /home/$SUDO_USER
+	cp .bashrc /home/$SUDO_USER
+}
 
-# Create menu entry
-desContent="[Desktop Entry]
-Name=Toggle Battery Mode
-Type=Application
-Icon=/home/$SUDO_USER/.local/share/icons/battery.png
-Exec=toggleConservationMode"
+configureKiller(){
+	# Program killer using name-----------------------------------------------------------
+	# Usage: killProgram <process_name>
+	echo "ps -A | grep $1 | xargs kill 2> /dev/null" > /usr/local/bin/killProgram
+	chmod +x /usr/local/bin/killProgram
+}
 
-echo $desContent > /home/$SUDO_USER/.local/share/applications/ToggleBtteryMode.desktop
-mkdir /home/$SUDO_USER/.local/share/icons
-cp assets/battery.png /home/$SUDO_USER/.local/share/icons/
+configureConservationMode(){
+	# Conservation mode toggler------------------------------------------------------------
+	g++ $(src_dir)/toggleConservation.cpp -o /usr/local/bin
+	sudo chown root toggleConservationMode
+	sudo chmod u+s toggleConservationMode
 
-# Initiate conservation mode
-toggleConservationMode
+	# Create menu entry
+	desContent="[Desktop Entry]
+	Name=Toggle Battery Mode
+	Type=Application
+	Icon=/home/$SUDO_USER/.local/share/icons/battery.png
+	Exec=toggleConservationMode"
 
-# Keyboard Backlight toggler(For Arch)--------------------------------------------------
-g++ $(src_dir)/toggleBacklight.cpp -o /usr/local/bin
-sudo chown root toggleBacklight
-sudo chmod u+s toggleBacklight
+	echo $desContent > /home/$SUDO_USER/.local/share/applications/ToggleBtteryMode.desktop
+	mkdir /home/$SUDO_USER/.local/share/icons
+	cp assets/battery.png /home/$SUDO_USER/.local/share/icons/
 
-# Turn the backlight on
-toggleBacklight
+	# Initiate conservation mode
+	toggleConservationMode
+}
+configureBacklight(){
+	# Keyboard Backlight toggler(For Arch)--------------------------------------------------
+	g++ $(src_dir)/toggleBacklight.cpp -o /usr/local/bin
+	sudo chown root toggleBacklight
+	sudo chmod u+s toggleBacklight
 
-# Create a menu entry
-desContent="[Desktop Entry]
-Name=Toggle Backlight
-Type=Application
-Icon=/home/$SUDO_USER/.local/share/icons/rgb_keyboard.png
-Exec=toggleBacklight"
+	# Turn the backlight on
+	toggleBacklight
 
-echo $desContent > /home/$SUDO_USER/.local/share/applications/ToggleBacklight.desktop
-cp assets/rgb_keyboard.png /home/$SUDO_USER/.local/share/icons/
+	# Create a menu entry
+	desContent="[Desktop Entry]
+	Name=Toggle Backlight
+	Type=Application
+	Icon=/home/$SUDO_USER/.local/share/icons/rgb_keyboard.png
+	Exec=toggleBacklight"
 
+	echo $desContent > /home/$SUDO_USER/.local/share/applications/ToggleBacklight.desktop
+	cp assets/rgb_keyboard.png /home/$SUDO_USER/.local/share/icons/
+}
+configureSSH(){
+	# SSH Configuration---------------------------------------------------------------------
+	echo "Do you want to configure openssh?(y/n)"
+	read ans
+	if [ $ans = 'y' ]; then
+		
+		echo "[+] Installing ssh"
+		sudo pacman -S openssh --noconfirm
+		
+		# echo "[+] Not enabling ssh at system start."
+		sudo systemctl enable sshd
 
-# SSH Configuration---------------------------------------------------------------------
-echo "Do you want to configure openssh?(y/n)"
-read ans
-if [ $ans = 'y' ]; then
-	
-	echo "[+] Installing ssh"
-	sudo pacman -S openssh --noconfirm
-	
-	# echo "[+] Not enabling ssh at system start."
-	sudo systemctl enable sshd
+		# Preparing directories
+		mkdir /home/$SUDO_USER/.ssh 2> /dev/null
 
-	# Preparing directories
-	mkdir /home/$SUDO_USER/.ssh 2> /dev/null
+		
+		echo "[+] Generating secret keys"
+		ssh-keygen
+		cat /$USER/.ssh/id_rsa.pub > /home/$SUDO_USER/.ssh/authorized_keys
+		rm /$USER/.ssh/id_rsa.pub
+		mv /$USER/.ssh/id_rsa /home/$SUDO_USER/.ssh/
+		echo "Now move the /home/$SUDO_USER/.ssh/id_rsa to a secure client machine"
 
-	
-	echo "[+] Generating secret keys"
-	ssh-keygen
-	cat /$USER/.ssh/id_rsa.pub > /home/$SUDO_USER/.ssh/authorized_keys
-	rm /$USER/.ssh/id_rsa.pub
-	mv /$USER/.ssh/id_rsa /home/$SUDO_USER/.ssh/
-	echo "Now move the /home/$SUDO_USER/.ssh/id_rsa to a secure client machine"
+		echo "Updating sshd configuration file"
+		sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
-	echo "Updating sshd configuration file"
-	sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+		echo "The default port is 22 (Unchanged) change it?(y/n)"
+		read op
+		if [ [op=="y"] ]; then
+			while : ; do
+				echo "Enter port number:"
+				read portNum
+				if timeout 1 bash -c "echo testing > /dev/tcp/google.com/$portNum"; then
+					echo "Port already in use. Enter another port."
+				else
+					sed -i "s/^#Port .*/Port $portNum/" /etc/ssh/sshd_config
+					break
+				fi
+			done
+		fi
 
-	echo "The default port is 22 (Unchanged) change it?(y/n)"
-	read op
-	if [ [op=="y"] ]; then
-		while : ; do
-			echo "Enter port number:"
-			read portNum
-			if timeout 1 bash -c "echo testing > /dev/tcp/google.com/$portNum"; then
-				echo "Port already in use. Enter another port."
-			else
-				sed -i "s/^#Port .*/Port $portNum/" /etc/ssh/sshd_config
-				break
-			fi
-		done
+		sudo systemctl start sshd
+
+		echo "[+] SSH configuration success!"
 	fi
+}
 
-	sudo systemctl start sshd
-
-	echo "[+] SSH configuration success!"
-fi
+placeFiles
+configureKiller
+configureConservationMode
+configureBacklight
+configureSSH
 
 # Local port forwarding for portmap rule, I have that in my .bashrc file
-sudo pacman -S socat openvpn jre11-openjdk-headless --noconfirm
+sudo pacman -Syyu ${packages_pacman[@]} --noconfirm

08f3e833284539e6beaabe75a1b99f759ed17e90
needl3 <anishchapagai0@gmail.com>
2021-11-19 20:26:49 +0545

    added network monitor and started organizing dwm and its components in seperate directories

--- dwm_collection/dwmblocks/modules/network.sh
+++ dwm_collection/dwmblocks/modules/network.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 
 upicon=
 downicon=
@@ -6,4 +7,48 @@ upinfo=
 downinfo=
 
 
-printf "$upicon $upinfo $downicon $downinfo"
+sleep_time=5
+
+# Check if system is freshly booted and create temp network counter files
+
+if [ ! -f /tmp/rx ];then
+	echo 0 > /tmp/rx
+	echo 0 > /tmp/tx
+fi
+
+#####################
+#	Download Speed	#
+#####################
+
+prev_B=$(cat /tmp/rx)
+current_B=$(cat /sys/class/net/wlan0/statistics/rx_bytes)
+
+echo $current_B > /tmp/rx
+
+received_B=$(expr $current_B - $prev_B)
+
+if [ $(expr $received_B / 1024)  -gt 1024 ]
+then
+	downinfo="$(expr $received_B / $(expr $sleep_time \* 1024 \* 1024))MB/s"
+else
+	downinfo="$(expr $received_B / 1024 )KB/s"
+fi
+
+
+#####################
+#	Upload Speed	#
+#####################
+prev_B=$(cat /tmp/tx)
+current_B=$(cat /sys/class/net/wlan0/statistics/tx_bytes)
+
+echo $current_B > /tmp/tx
+
+received_B=$(expr $current_B - $prev_B)
+
+if [ $(expr $received_B / 1024)  -gt 1024 ];then
+	upinfo="$(expr $received_B / $(expr $sleep_time \* 1024 \* 1024))MB/s"
+else
+	upinfo="$(expr $received_B / 1024 )KB/s"
+fi
+
+printf "$upicon $upinfo $downicon $downinfo\n"

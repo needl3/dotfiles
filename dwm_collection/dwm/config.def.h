5e1d7ce38d5de6cb398978615b52342e57aae42e
needl3 <anishchapagai0@gmail.com>
2021-11-20 22:51:29 +0545

    dmwnu status abr padding and color patch applied

--- dwm_collection/dwm/config.def.h
+++ dwm_collection/dwm/config.def.h
@@ -5,18 +5,26 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 0;       /* vertical padding of bar */
+static const int sidepad            = 20;       /* horizontal padding of bar */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=12:", "JetBrainsMono Nerd Font;style=12" };
+static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:", "JetBrainsMono Nerd Font;style=12" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font;style=15";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#111111";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#bbbbbb";
+static const char col_purple1[]     = "#280080";
+static const char col_cyan[]        = "#008080";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               			fg 		        bg  	       border   */
+	[SchemeNorm] 		= 	{ col_gray3,	 col_gray1, 	col_gray2 },
+	[SchemeSel]  		= 	{ col_gray4,	 "#000000",  	col_cyan  },
+	[SchemeStatus]  	= 	{ "#ffffff",	 col_gray1,  	"#111111"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  	= 	{ col_gray4,	 col_purple1,  	"#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+    [SchemeTagsNorm]  	= 	{ "#ffffff",	 col_gray1,  	"#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]  	= 	{ "#ffffff",	 col_cyan,  	"#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoNorm]  	= 	{ col_gray3,	 col_gray1,  	"#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */

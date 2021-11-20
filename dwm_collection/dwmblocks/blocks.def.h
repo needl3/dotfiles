eb695d3ee424c995d1e16cfbebfb0de5d800c2db
needl3 <anishchapagai0@gmail.com>
2021-11-20 11:01:09 +0545

    installation for dwmblocks in global scope

--- dwm_collection/dwmblocks/blocks.def.h
+++ dwm_collection/dwmblocks/blocks.def.h
@@ -1,15 +1,21 @@
+
+//
+//	Add this line in your .bashrc file
+//	export PATH="~/.dblocks_modules/:$PATH"
+//
+
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/					/*Update Interval*/	/*Update Signal*/
-	{"", 		"modules/backlight.sh",				0,					0},
-	{"", 		"modules/volume.sh",				0,					1},
-	{"", 		"modules/cpu.sh",					5,					2},
-	{"", 		"modules/memory.sh",				5,					3},
-	{"", 		"modules/network.sh",				5,					4},
-	{"",		"modules/updates.sh",				0,					5},
-	{"", 		"modules/battery.sh",				120,				6},
-	{"", 		"modules/date.sh",					60,					7},
-	{"", 		"modules/wifi.sh",					5,					8},
+	{"", 		"backlight.sh",				0,					0},
+	{"", 		"volume.sh",				0,					1},
+	{"", 		"cpu.sh",					5,					2},
+	{"", 		"memory.sh",				5,					3},
+	{"", 		"network.sh",				5,					4},
+	{"",		"updates.sh",				0,					5},
+	{"", 		"battery.sh",				120,				6},
+	{"", 		"date.sh",					60,					7},
+	{"", 		"wifi.sh",					5,					8},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
--- dwm_collection/dwmblocks/modules/volume.sh
+++ dwm_collection/dwmblocks/modules/volume.sh
@@ -1,8 +1,8 @@
 
-vol_icons=(   )
+vol_icons=(🔇   )
 
-vol_magnitude=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
+vol_magnitude=$(pactl list sinks | awk 'FNR == 10 {print $5}' | grep -o '[0-9]*')
 
-icon=${vol_icons[$(expr $vol_magnitude % 3)]}
+icon=${vol_icons[$(expr $vol_magnitude / 25)]}
 
-printf " $icon $vol_magnitude"
+printf "$icon $vol_magnitude%%"

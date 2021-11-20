4d56c7bbcefe8db03aecd899f783c937051c8b98
needl3 <anishchapagai0@gmail.com>
2021-11-20 13:07:42 +0545

    Discarded the path implementation

--- dwm_collection/dwmblocks/blocks.def.h
+++ dwm_collection/dwmblocks/blocks.def.h
@@ -1,21 +1,15 @@
-
-//
-//	Add this line in your .bashrc file
-//	export PATH="~/.dblocks_modules/:$PATH"
-//
-
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/					/*Update Interval*/	/*Update Signal*/
-	{"", 		"backlight.sh",				0,					0},
-	{"", 		"volume.sh",				0,					1},
-	{"", 		"cpu.sh",					5,					2},
-	{"", 		"memory.sh",				5,					3},
-	{"", 		"network.sh",				5,					4},
-	{"",		"updates.sh",				0,					5},
-	{"", 		"battery.sh",				120,				6},
-	{"", 		"date.sh",					60,					7},
-	{"", 		"wifi.sh",					5,					8},
+	/*Icon*/	/*Command*/								/*Update Interval*/	/*Update Signal*/
+	{"", 		"/opt/dblocks_modules/backlight.sh",				0,					0},
+	{"", 		"/opt/dblocks_modules/volume.sh",					0,					1},
+	{"", 		"/opt/dblocks_modules/cpu.sh",						5,					2},
+	{"", 		"/opt/dblocks_modules/memory.sh",					5,					3},
+	{"", 		"/opt/dblocks_modules/network.sh",					5,					4},
+	{"",		"/opt/dblocks_modules/updates.sh",					0,					5},
+	{"", 		"/opt/dblocks_modules/battery.sh",					120,				6},
+	{"", 		"/opt/dblocks_modules/date.sh",						60,					7},
+	{"", 		"/opt/dblocks_modules/wifi.sh",						5,					8},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

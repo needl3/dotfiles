06cf7d2a8e9d949f1b27373da4929e0eba3da4ce
needl3 <anishchapagai0@gmail.com>
2021-11-22 22:29:38 +0545

    Wifi status bar and script added

--- dwm_collection/dwmblocks/modules/backlight.sh
+++ dwm_collection/dwmblocks/modules/backlight.sh
@@ -3,9 +3,11 @@ icon=💡
 
 info=$(cat /sys/class/backlight/intel_backlight/brightness)
 
-if [ $(( $info / 900  )) ]
+if [ $(( $info / 900  )) -eq 1 ]
 then
 	info=100
+else
+	info=$(( $info / 10 ))
 fi
 
 printf "$icon $info%%"

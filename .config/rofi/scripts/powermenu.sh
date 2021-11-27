e5a28bf13000dcd27b011a4a8c8877ca0d9009b8
needl3 <anishchapagai0@gmail.com>
2021-11-27 19:48:37 +0545

    file structure change

---
+++ .config/rofi/scripts/powermenu.sh
@@ -0,0 +1,28 @@
+shutdown="    Shutdown"
+reboot="    Reboot"
+sleep="⏾     Sleep"
+logout="    Logout"
+
+selected_option=$(echo "$shutdown
+$reboot
+$sleep
+$logout" | rofi -dmenu\
+                  -i\
+                  -l 4\
+                  -p "⏻")
+
+if [ "$selected_option" == "$logout" ]
+then
+    killall -u $(whoami)
+elif [ "$selected_option" == "$shutdown" ]
+then
+    systemctl poweroff
+elif [ "$selected_option" == "$reboot" ]
+then
+    systemctl reboot
+elif [ "$selected_option" == "$sleep" ]
+then
+    systemctl suspend
+else
+    echo "No match" 
+fi

eef8e95f7ebf3698c6ebdff5be37c097f828395a
needl3 <anishchapagai0@gmail.com>
2022-01-08 13:50:35 +0545

    local ip on the bar

---
+++ dwm_collection/dwmblocks/modules/bar/ip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+icon=📨
+findActiveInterface()
+{
+	intfr=
+	for i in $( ip a list | grep BROADCAST | awk '{print $2}' | grep -o 'wlan[0-9]*');
+	do
+		if iwctl station $i show | grep disconnected > /dev/null;then
+			continue
+		else
+			intfr=$i
+		fi
+	done
+	if [ -z $intfr ];then
+		intfr=wlan0
+	fi
+	printf $intfr
+}
+
+intfr=$(findActiveInterface)
+ip=$(iwctl station $intfr show | grep IPv4 | cut -c 33-47)
+if [ -z ip ];then
+	ip="XXX.XXX.XXX.XXX"
+fi
+printf "$icon$ip "

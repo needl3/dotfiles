ea58d82c4ae61756aa764a5085a76065c7a1fdaf
needl3 <anishchapagai0@gmail.com>
2022-01-11 11:59:57 +0545

    dwm alpha patch for border transparency issue

--- dwm_collection/dwm/config.def.h
+++ dwm_collection/dwm/config.def.h
@@ -1,23 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 0;       /* vertical padding of bar */
+static const int vertpad            = 2;       /* vertical padding of bar */
 static const int sidepad            = 0;       /* horizontal padding of bar */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 
-static const char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=12:" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "monospace:size=12", "JoyPixels:pixelsize=12:" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font;style=15";
 
+static const char col_black[] 		= "#000000";
+static const char col_white[] 		= "#ffffff";
 #include "../dwmbar/DwmbarThemes.h"
+
+static const unsigned int baralpha = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, OPAQUE },
+	[SchemeSel]  = { OPAQUE, baralpha, OPAQUE },
+};
 
 /* tagging */
 static const char *tags[] = { "💻", "🐚", "🌏", "🎶", "📺", "💬"};

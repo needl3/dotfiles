c9ffefb3e36324e16cce6c35f2f62cbea23677d3
needl3 <anishchapagai0@gmail.com>
2022-02-15 20:16:17 +0545

    UwU

--- dwm_collection/dwmblocks/modules/bar/battery.sh
+++ dwm_collection/dwmblocks/modules/bar/battery.sh
@@ -14,8 +14,9 @@ case $(cat $bat_dir/status) in
         battery_icon=${icons[$(expr $battery_percent % 4)]}
         if [ $battery_percent -lt 20 ]
         then
-            notify-send "Low battery!
-            Computer will shutdown at 15%" -u critical
+             notify-send "Low battery!
+  Computer will shutdown at 15%" -u critical -i /usr/share/icons/Adwaita/32x32/status/battery-level-20-symbolic.symbolic.png
+            canberra-gtk-play -i audio-volume-change -d "LowBattery"
 
             if [ $battery_percent -lt 15 ]
             then

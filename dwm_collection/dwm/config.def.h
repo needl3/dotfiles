4860b762a2cea185efc6f443f5ea8932b5087516
needl3 <anishchapagai0@gmail.com>
2022-02-20 09:46:15 +0545

    autostart dwm on login, wallpaper shortcuts changed

--- dwm_collection/dwm/config.def.h
+++ dwm_collection/dwm/config.def.h
@@ -158,8 +158,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,             			XK_Right,  spawn,          SHCMD("~/.config/scripts/wallpaper_toggler.sh 1") },
-	{ MODKEY,             			XK_Left,   spawn,          SHCMD("~/.config/scripts/wallpaper_toggler.sh -1") },
+	{ MODKEY|ShiftMask,				XK_Right,  spawn,          SHCMD("~/.config/scripts/wallpaper_toggler.sh 1") },
+	{ MODKEY|ShiftMask,				XK_Left,   spawn,          SHCMD("~/.config/scripts/wallpaper_toggler.sh -1") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)

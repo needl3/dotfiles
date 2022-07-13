bee98f01de589a3c12b56b53ec53384bb365ac3f
needl3 <57314527+needl3@users.noreply.github.com>
2022-07-13 13:27:22 +0545

    external monitor managing script

---
+++ src/extendMonitor.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+RED=$'\e[31m'
+YELLOW=$'\e[33m'
+GREEN=$'\e[32m'
+ENDCOLOR=$'\e[0m'
+
+if ! xrandr -v &> /dev/null;then
+	echo "${RED}[X] XRANDR not installed${ENDCOLOR}" 
+	exit 1
+fi
+# Get active displays
+DISPLAYS=()
+xrandr --listactivemonitors | awk 'FNR>1 {print $4}' | while read l; 
+do
+	DISPLAYS+=("$l")
+done
+
+PRIMARY="$(xrandr --listactivemonitors | awk 'FNR==2 {print $4}')"
+SECONDARY="$(xrandr --listactivemonitors | awk 'FNR==3 {print $4}')"
+[[ $SECONDARY == "" ]] && SECONDARY="HDMI1"
+xrandr --output "$SECONDARY" --auto
+
+if [[ $1 == "r" ]];then
+	xrandr --output "$PRIMARY" --left-of "$SECONDARY"
+elif [[ $1 == "l" ]];then
+	xrandr --output "$PRIMARY" --right-of "$SECONDARY"
+elif [[ $1 == "u" ]];then
+	xrandr --output "$PRIMARY" --below "$SECONDARY"
+elif [[ $1 == "d" ]];then
+	xrandr --output "$PRIMARY" --above "$SECONDARY"
+elif [[ $1 == "k" ]];then
+	xrandr --output "$SECONDARY" --off
+else
+	echo "${YELLOW}Usage: ./extendMonitor.sh {l/r/u/d}${ENDCOLOR}"
+	if notify-send -v &> /dev/null;then
+		notify-send "Failed to extend to $1"
+		exit 1
+	fi
+fi
+

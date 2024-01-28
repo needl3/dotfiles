9b80ee88cda88ece64dfeb76cea2b4f7319ed5c0
needl3 <57314527+needl3@users.noreply.github.com>
2024-01-28 20:43:32 +0545

    feat: add new configs

---
+++ .config/wofi/power/launch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+shutdown="<span size='0.001pt'>Shutdown</span><span size='30pt'>󰐥</span>"
+reboot="<span size='0.001pt'>Reboot</span><span size='30pt'></span>"
+sleep="<span size='0.001pt'>Sleep</span><span size='30pt'>⏾</span>"
+logout="<span size='0.001pt'>Logout</span><span size='30pt'>󰗽</span>"
+lock="<span size='0.001pt'>Lock</span><span size='30pt'></span>"
+
+actions="$shutdown\n$reboot\n$sleep\n$logout\n$lock"
+
+selected="$(echo -e "$actions"| wofi --conf /home/siyo/.config/wofi/power/config)"
+
+# if no options selected
+if test -z "$selected"; then
+    exit
+fi
+
+case $selected in
+        $shutdown)
+		        systemctl poweroff;;
+        $reboot)
+			systemctl reboot;;
+        $sleep)
+			systemctl suspend;;
+        $logout)
+			killall -u siyo;;
+        $lock)
+                swaylock;;
+        *)
+                ;;
+esac

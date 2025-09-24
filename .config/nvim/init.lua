a1fee682bf1d648d61b6a69a606d9a37ed2998db
Anish Chapagai <57314527+needl3@users.noreply.github.com>
2025-09-24 15:47:45 +0545

    feat: dap for go debugging

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,4 @@ require("config.settings")
 -------------------- Set default colorscheme --------------------------
 local default_colorscheme = require("config.utils").colorscheme.default
 vim.cmd.colorscheme(default_colorscheme)
+vim.fn.setenv("PATH", os.getenv("PATH") .. ":" .. vim.fn.expand("$HOME/go/bin"))
